47dbd1ff49b2ced50c299442bbf7e8205ab84aa1
Shubham Sabale <shubhamsabale39@gmail.com>
2025-08-02 10:15:13 +0000

    File changes

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,16 @@
+
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: 'village-eats-et2ke',
-  appId: '1:568284241701:web:ed7843414e0629b04584cc',
-  storageBucket: 'village-eats-et2ke.firebasestorage.app',
-  apiKey: 'AIzaSyDAsJm9ANN3W4YHwDox71xqJCKFb5TOUgY',
-  authDomain: 'village-eats-et2ke.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '568284241701',
+  "projectId": "village-eats-et2ke",
+  "appId": "1:568284241701:web:ed7843414e0629b04584cc",
+  "storageBucket": "village-eats-et2ke.firebasestorage.app",
+  "apiKey": "AIzaSyDAsJm9ANN3W4YHwDox71xqJCKFb5TOUgY",
+  "authDomain": "village-eats-et2ke.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "568284241701"
 };
 
 // Initialize Firebase
