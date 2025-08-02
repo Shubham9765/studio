59c84e97e52942bf974c98fd66c5f6c584b5ea9e
Shubham Sabale <shubhamsabale39@gmail.com>
2025-08-02 10:10:10 +0000

    Error: Failed to execute 'subscribe' on 'PushManager': Subscription fail

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
@@ -6,6 +7,7 @@ const pwaConfig = withPWA({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  sw: 'firebase-messaging-sw.js', // Use our custom service worker
 });
 
 
