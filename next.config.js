2efbd2a06acb0df37917729ef81d43b335dd4c58
Shubham Sabale <shubhamsabale39@gmail.com>
2025-09-04 11:57:18 +0000

    photo is not loading and not showing (_for element src/app/grocery/store

--- next.config.js
+++ next.config.js
@@ -24,6 +24,12 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: '**',
       }
     ],
